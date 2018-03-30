55af7ff3783a033c82da98af2bbf381ed190ec4f
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-03-29 19:43:07 -0600

    minor changes

--- filter.js
+++ filter.js
@@ -1,11 +1,13 @@
 // From the numbers array, store even numbers in a variable  
 
-console.log(' ============= Create numbers array with spread operator ==============')
+console.log('====== Create numbers array with spread operator =====')
 const numbers = [...Array(10).keys()];
 console.log(`The numbers array is: ${numbers}`);
 
-console.log('=============== With for of loop ==================');
-const evenNumbers = [];
+
+
+console.log('====== Style 1: With for of loop ======');
+let evenNumbers = [];
 
 for (let number of numbers) {
   if (number % 2 === 0) {
@@ -14,7 +16,9 @@ for (let number of numbers) {
 }
 console.log(`The even numbers are: ${evenNumbers}`);
 
-console.log('============ With for loop and mutations =============');
+
+
+console.log('====== Style 2: With for loop and mutations ======');
 for (let index = 0; index < numbers.length; index++) {
   if( numbers[index] % 2 !== 0 ) {
     numbers.splice(index,1)
@@ -23,8 +27,9 @@ for (let index = 0; index < numbers.length; index++) {
 console.log(`The even numbers are: ${numbers}`);
 
 
-console.log('============ With filter =============');
+
+console.log('======= Style 3: With filter ========');
 const isEven = number => number % 2 === 0;
 
-const x = numbers.filter(isEven);
-console.log(`The even numbers are: ${x}`);
+evenNumbers = numbers.filter(isEven);
+console.log(`The even numbers are: ${evenNumbers}`);
