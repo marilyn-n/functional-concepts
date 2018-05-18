72131d5d56d6b2d1fd8ca8f99b0d17b715875301
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-05-18 18:49:27 -0500

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,4 +1,17 @@
-//============= Calculating a Sum
+// The most flexible method is reduce, which can be used to create map and filter methods
+
+
+const filter = (fn, arr) => arr.reduce((newArr, item) => {
+  return fn(item) ? newArr.concat([item]) : newArr;
+}, []);
+
+
+const map = (fn, arr) => arr.reduce((acc, item, index, arr) => {
+  return acc.concat(fn(item, index, arr));
+}, []);
+
+
+//============= Example: Calculating a Sum
 
 const numbers = [...Array(12).keys()];
 
